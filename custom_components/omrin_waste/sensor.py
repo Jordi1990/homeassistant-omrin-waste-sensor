fc7409e273359e3f07d8fcd914da31e7b9f2fbcc
Jordi Epema <jordi.epema@gmail.com>
2020-02-16 20:18:26 +0100

    Fixed bug

--- custom_components/omrin_waste/sensor.py
+++ custom_components/omrin_waste/sensor.py
@@ -77,7 +77,7 @@ def fetchCalendar(publicKey, postalCode, houseNumber):
     typeToEmptyToday = getEmptyTypeOnDate(calendar, datetime.datetime.now())
     typeToEmptyTomorrow = getEmptyTypeOnDate(calendar, datetime.datetime.now() + datetime.timedelta(days=1))
     nextEmptyDate = getNextEmptyDate(calendar)
-    typeToEmptyNext = getEmptyTypeOnDate(calendar, datetime.datetime.strptime(nextEmptyDate, '%Y-%m-%dT%H:%M:%S'))
+    typeToEmptyNext = getEmptyTypeOnDate(calendar, datetime.datetime.strptime(nextEmptyDate, '%Y-%m-%d'))
     
     return {'biobak': nextBiobak, 'sortibak': nextSortibak, 'papierbak': nextPapierbak, 'today': typeToEmptyToday, 'tomorrow': typeToEmptyTomorrow, 'nextdate': nextEmptyDate, 'next': typeToEmptyNext}
 
