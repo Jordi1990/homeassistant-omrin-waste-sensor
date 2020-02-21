69ee00b90f4a9288d67c19678d56f5d9138fb6f5
Jordi Epema <jordi.epema@gmail.com>
2020-02-21 20:28:02 +0100

    Changed update frequency to 1 hour

--- custom_components/omrin_waste/sensor.py
+++ custom_components/omrin_waste/sensor.py
@@ -24,37 +24,46 @@ _LOGGER = logging.getLogger(__name__)
 # Generate unique id
 appId = uuid.uuid1().__str__()
 
+
 # Post helper method
-def doPost(url, data = None, jsonData = None):
-    if(data is None):    
+def doPost(url, data=None, jsonData=None):
+    if (data is None):
         request = requests.post(url, json=jsonData)
     else:
         request = requests.post(url, data=data)
-        
+
     if request.status_code != 200:
-        raise requests.RequestException("Request failed with status code {0} and reason {1}".format(request.status_code, request.reason))
+        raise requests.RequestException(
+            "Request failed with status code {0} and reason {1}".format(request.status_code, request.reason))
     return json.loads(request.text)
+
 
 # Retrieve public key from API used to sign requests
 def fetchPublicKey():
-    response = doPost("https://api-omrin.freed.nl//Account/GetToken/", jsonData={'AppId': appId, 'AppVersion': '', 'OsVersion': '', 'Platform': ''})
+    response = doPost("https://api-omrin.freed.nl//Account/GetToken/",
+                      jsonData={'AppId': appId, 'AppVersion': '', 'OsVersion': '', 'Platform': ''})
     return b64decode(response['PublicKey'])
 
-def getNextEmptyDate(calendar, type = None):
-    if(type is None):
+
+# Retrieve next empty date
+def getNextEmptyDate(calendar, type=None):
+    if (type is None):
         nextEmptyDate = next(iter(calendar), None)
     else:
-        nextEmptyDate = next(filter(lambda x:x["Omschrijving"]==type, calendar), None)
+        nextEmptyDate = next(filter(lambda x: x["Omschrijving"] == type, calendar), None)
     datetimeObj = datetime.datetime.strptime(nextEmptyDate['Datum'], '%Y-%m-%dT%H:%M:%S')
-    return datetimeObj.strftime('%Y-%m-%d') 
+    return datetimeObj.strftime('%Y-%m-%d')
 
+
+# Retrieve container type on certain date
 def getEmptyTypeOnDate(calendar, date):
     date = date.strftime('%Y-%m-%dT00:00:00')
-    emptyEvent = next(filter(lambda x:x["Datum"]==date, calendar), None)
+    emptyEvent = next(filter(lambda x: x["Datum"] == date, calendar), None)
     if emptyEvent is not None:
         return emptyEvent['Omschrijving']
     else:
         return None
+
 
 # Retrieve calendar for given address
 def fetchCalendar(publicKey, postalCode, houseNumber):
@@ -63,14 +72,16 @@ def fetchCalendar(publicKey, postalCode, houseNumber):
 
     # Sign request with public key
     encryptedRequest = pkcs1.encrypt(json.dumps(requestBody).encode(),
-        rsaPublicKey)
+                                     rsaPublicKey)
     # Encode request
     base64EncodedRequest = b64encode(encryptedRequest).decode("utf-8")
 
-    response = doPost("https://api-omrin.freed.nl//Account/FetchAccount/" + appId, data='"' + base64EncodedRequest + '"')   
-    
-    calendar = response['CalendarHomeV2']
-    
+    response = doPost("https://api-omrin.freed.nl//Account/FetchAccount/" + appId,
+                      data='"' + base64EncodedRequest + '"')
+
+    return response['CalendarHomeV2']
+
+def parseCalendar(calendar):
     nextBiobak = getNextEmptyDate(calendar, 'Biobak')
     nextSortibak = getNextEmptyDate(calendar, 'Sortibak')
     nextPapierbak = getNextEmptyDate(calendar, 'Papierbak')
@@ -78,49 +89,53 @@ def fetchCalendar(publicKey, postalCode, houseNumber):
     typeToEmptyTomorrow = getEmptyTypeOnDate(calendar, datetime.datetime.now() + datetime.timedelta(days=1))
     nextEmptyDate = getNextEmptyDate(calendar)
     typeToEmptyNext = getEmptyTypeOnDate(calendar, datetime.datetime.strptime(nextEmptyDate, '%Y-%m-%d'))
-    
-    return {'biobak': nextBiobak, 'sortibak': nextSortibak, 'papierbak': nextPapierbak, 'today': typeToEmptyToday, 'tomorrow': typeToEmptyTomorrow, 'nextdate': nextEmptyDate, 'next': typeToEmptyNext}
+
+    return {'biobak': nextBiobak, 'sortibak': nextSortibak, 'papierbak': nextPapierbak, 'today': typeToEmptyToday,
+            'tomorrow': typeToEmptyTomorrow, 'nextdate': nextEmptyDate, 'next': typeToEmptyNext}
+
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-     _LOGGER.debug("Setting omrin waste sensor")
-     publicKey = await hass.async_add_executor_job(fetchPublicKey)
+    _LOGGER.debug("Setting omrin waste sensor")
+    publicKey = await hass.async_add_executor_job(fetchPublicKey)
 
-     async def async_update_data():
-        try:            
+    async def async_update_data():
+        try:
             postalcode = config.get(CONF_POSTALCODE)
             housenumber = config.get(CONF_HOUSENUMBER)
 
             # Run fetchCalendar async
-            return await hass.async_add_executor_job(fetchCalendar, publicKey, postalcode, housenumber)
+            calendar = await hass.async_add_executor_job(fetchCalendar, publicKey, postalcode, housenumber)
+            # TODO: Only run parse calendar every hour and fetch calendar once a day
+            return await hass.async_add_executor_job(parseCalendar, calendar)
         except requests.RequestException:
             raise UpdateFailed
 
-     coordinator = DataUpdateCoordinator(
+    coordinator = DataUpdateCoordinator(
         hass,
         _LOGGER,
         name="sensor",
         update_method=async_update_data,
-        update_interval=datetime.timedelta(hours=12),
+        update_interval=datetime.timedelta(hours=1),
         request_refresh_debouncer=debounce.Debouncer(
-                hass,
-                _LOGGER,
-                REQUEST_REFRESH_DEFAULT_COOLDOWN,
-                REQUEST_REFRESH_DEFAULT_IMMEDIATE,
-            )
-     )
-     
-     types = ['biobak', 'sortibak', 'papierbak', 'today', 'tomorrow', 'nextdate', 'next']
+            hass,
+            _LOGGER,
+            REQUEST_REFRESH_DEFAULT_COOLDOWN,
+            REQUEST_REFRESH_DEFAULT_IMMEDIATE,
+        )
+    )
 
-     # Fetch initial data
-     await coordinator.async_refresh()
+    types = ['biobak', 'sortibak', 'papierbak', 'today', 'tomorrow', 'nextdate', 'next']
 
-     async_add_entities(WasteEmptyDateSensor(coordinator, type) for idx, type
+    # Fetch initial data
+    await coordinator.async_refresh()
+
+    async_add_entities(WasteEmptyDateSensor(coordinator, type) for idx, type
                        in enumerate(types))
 
 
 class WasteEmptyDateSensor(Entity):
     _state = None
-    
+
     def __init__(self, coordinator, type):
         self.coordinator = coordinator
         self._type = type
@@ -139,9 +154,9 @@ class WasteEmptyDateSensor(Entity):
         """No need to poll. Coordinator notifies entity of updates."""
         return False
 
-    async def async_update(self):    
+    async def async_update(self):
         await self.coordinator.async_request_refresh()
-        
+
     async def async_added_to_hass(self):
         self.coordinator.async_add_listener(
             self.async_write_ha_state
